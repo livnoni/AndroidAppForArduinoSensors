1ba80e39cb03b5aec005bd9cde7f6eb7ba1b15c4
livnoni <yehuda.livnoni@gmail.com>
2017-05-04 21:20:22 +0300

    Arduino sample code

---
+++ SimpleDistanceHC-SR04.ino
@@ -0,0 +1,31 @@
+#include <NewPing.h>
+
+#define TRIGGER_PIN  14
+#define ECHO_PIN     15
+#define MAX_DISTANCE 1000
+
+
+
+NewPing DistanceSensor(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(12, OUTPUT);
+}
+
+void loop()
+{
+   int cm = DistanceSensor.ping_cm();
+
+   //Serial.print("Distance: ");
+   Serial.print(cm);
+   Serial.println("cm");
+
+   delay(500);
+ 
+    
+    
+}
+
+
